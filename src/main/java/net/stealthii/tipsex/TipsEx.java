1be20b0f5fb40d972ed79fd67f263bd93e8f690f
Stealthii <dpreid@gmail.com>
2011-09-25 15:55:41 +0100

    Converted to Maven Project

---
+++ src/main/java/net/stealthii/tipsex/TipsEx.java
@@ -0,0 +1,16 @@
+package net.stealthii.tipsex;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class TipsEx extends JavaPlugin {
+    public void onDisable() {
+        // TODO: Place any custom disable code here.
+        System.out.println(this + " is now disabled!");
+    }
+
+    public void onEnable() {
+        // TODO: Place any custom enable code here, such as registering events
+        
+        System.out.println(this + " is now enabled!");
+    }
+}
